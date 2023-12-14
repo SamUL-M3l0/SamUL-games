7bffa7c4416fc41ac9a6592bedfd6768638bda79
SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>
2023-12-14 07:48:06 -0500

    Delete cloud1.tsx
    Signed-off-by: SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>

--- cloud1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="cloud1" tilewidth="32" tileheight="32" tilecount="8" columns="4">
- <image source="../../Downloads/Asset/Items/cloud1.png" width="128" height="71"/>
-</tileset>
