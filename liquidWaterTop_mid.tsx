7a5375c955f07efba307c4305694be295e1d12a8
SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>
2023-12-14 07:45:57 -0500

    Delete liquidWaterTop_mid.tsx
    Signed-off-by: SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>

--- liquidWaterTop_mid.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="liquidWaterTop_mid" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../Downloads/Asset/Tiles/liquidWaterTop_mid.png" width="70" height="70"/>
-</tileset>
