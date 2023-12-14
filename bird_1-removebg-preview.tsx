c1621d1f93533914bba42eee880fc067025f7ee3
SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>
2023-12-14 07:53:03 -0500

    Delete bird_1-removebg-preview.tsx
    Signed-off-by: SamUL-M3l0 <152813487+SamUL-M3l0@users.noreply.github.com>

--- bird_1-removebg-preview.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="bird_1-removebg-preview" tilewidth="32" tileheight="32" tilecount="42" columns="7">
- <image source="../../Downloads/bird_1-removebg-preview.png" width="254" height="199"/>
-</tileset>
